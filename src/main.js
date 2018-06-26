099d6cd31c6c0e454979b3e50e8efaeba18bf893
yyy <178340042@qq.com>
2018-06-26 17:04:25 +0800

    123

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store/'
-import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-default/index.css';
-import 'element-ui/lib/theme-chalk/index.css';
+// import ElementUI from 'element-ui'
+// // import 'element-ui/lib/theme-default/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
+// Vue.use(ElementUI);
 
 new Vue({
 	el: '#app',
