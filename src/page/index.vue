19aa1b9535524f329e7c9676b461e8da6f4cd552
yyy <178340042@qq.com>
2018-06-26 16:50:59 +0800

    123

---
+++ src/page/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="fillcontain">
+        123
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data(){
+            return {
+
+            }
+        },
+        components: {
+
+        },
+        created(){
+        },
+        mounted(){
+
+        },
+        methods: {
+
+        },
+    }
+</script>
+
+<style lang="less">
+    @import '../style/mixin';
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,35 +3,35 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const login = r => require.ensure([], () => r(require('@/page/login')), 'login');
-const manage = r => require.ensure([], () => r(require('@/page/manage')), 'manage');
-const error = r => require.ensure([], () => r(require('@/page/error')), 'error');
-const phone_contact = r => require.ensure([], () => r(require('@/page_biz/phone_contact')), 'phone_contact');
+const index = r => require.ensure([], () => r(require('@/page/index')), 'index');
+// const manage = r => require.ensure([], () => r(require('@/page/manage')), 'manage');
+// const error = r => require.ensure([], () => r(require('@/page/error')), 'error');
+// const phone_contact = r => require.ensure([], () => r(require('@/page_biz/phone_contact')), 'phone_contact');
 
 
 const routes = [
 	{
 		path: '/',
-		component: login
+		component: index
 	},
-	{
-		path: '/manage',
-		component: manage,
-		name: '',
-		children: [
-			{
-				path: '/phone_contact',
-				component: phone_contact,
-				meta: ['电话联系', '慈溪电联记录'],
-			},
-			{
-				path: '/error',
-				component: error,
-				meta: ['错误', '错误'],
-			}
-
-		]
-	}
+	// {
+	// 	path: '/manage',
+	// 	component: manage,
+	// 	name: '',
+	// 	children: [
+	// 		{
+	// 			path: '/index',
+	// 			component: phone_contact,
+	// 			meta: ['电话联系', '慈溪电联记录'],
+	// 		},
+	// 		{
+	// 			path: '/error',
+	// 			component: error,
+	// 			meta: ['错误', '错误'],
+	// 		}
+    //
+	// 	]
+	// }
 ]
 
 export default new Router({
